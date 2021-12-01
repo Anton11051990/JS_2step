9c49f1fa9a6afd6284372d7249b98d7f4490fa47
Anton <Rogachev.a.e.90@mail.ru>
2021-12-01 21:48:44 +0300

    Добавление изображения, правка листа стайл

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 const products = [
-  { id: 1, title: "Notebook", price: 2000 },
-  { id: 2, title: "Mouse", price: 20 },
-  { id: 3, title: "Keyboard", price: 200 },
-  { id: 4, title: "Gamepad", price: 50 },
+  { id: 1, title: "Notebook", price: 2000, image: "image" },
+  { id: 2, title: "Mouse", price: 20, image: "image" },
+  { id: 3, title: "Keyboard", price: 20, image: "image" },
+  { id: 4, title: "Gamepad", price: 50, image: "image" },
 ];
 //Функция для формирования верстки каждого товара
 //Добавить в выводе изображение
-const renderProduct = (title, price) => {
+const renderProduct = (title, price, image) => {
   return `<div class="product-item">
                 <h3 class="title">${title}</h3>
+                <img src="images/imag.jpg" alt="">
                 <p class="price">${price}</p>
                 <button class="buy-btn">Купить</button>
             </div>`;
 };
 const renderPage = (list) => {
   const productsList = list.map((item) =>
-    renderProduct(item.title, item.price)
+    renderProduct(item.title, item.price, item.image)
   );
   console.log(productsList);
   document.querySelector(".products").innerHTML = productsList.join("");
